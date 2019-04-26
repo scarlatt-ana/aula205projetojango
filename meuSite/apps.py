0e93eb2362e983a66182d035b0860800d3fe9266
gitignore <scarlattaparecida@gmail.com>
2019-04-26 11:55:09 -0300

    gitignore

---
+++ meuSite/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MeusiteConfig(AppConfig):
+    name = 'meuSite'
---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\renomear.desktop-bs0u13s\desktop\python-aula204\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
